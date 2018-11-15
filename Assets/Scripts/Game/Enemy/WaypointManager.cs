3f018142da778ee0098017362fee1662b9ccfde3
Relag <44652125+Relag@users.noreply.github.com>
2018-11-15 10:59:55 -0500

    Created a script to manage enemy waypoints

--- Assets/Scripts/Game/Enemy/WaypointManager.cs
+++ Assets/Scripts/Game/Enemy/WaypointManager.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class WaypointManager : MonoBehaviour {
 
+    public static WaypointManager Instance;
+
+    public List<Path> Paths = new List<Path>();
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        Instance = this;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public Vector3 GetSpawnPosition(int pathIndex)
+    {
+        return Paths[pathIndex].Waypoint[0].position;
+    }
+}
+
+[System.Serializable]
+public class Path
+{
+    public List<Transform> Waypoint = new List<Transform>();
 }
